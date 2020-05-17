4b66fddd8c5644e9ed0e6d1f1187f34caa301fda
Cynthia Iloekwe <ciloekwe@seamfix.com>
2020-05-17 18:31:24 +0100

    login and signup form with angular material

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -5,6 +5,16 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import { MatButtonToggleModule, MatDatepickerModule, MatNativeDateModule, MatTooltipModule } from '@angular/material';
 
 
 
@@ -12,7 +22,21 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
   declarations: [AuthenticationComponent, LoginComponent, ForgotPasswordComponent],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonToggleModule,
+    MatTooltipModule
   ]
 })
 export class AuthenticationModule { }
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm:FormGroup;
+  Images = ["../../../assets/images/auth.svg", "../../../assets/images/data.svg","../../../assets/images/print.svg"];
+  hobbies = new FormControl();
+  hobbyList: string[] = ['Dance', 'Music', 'Reading', 'Sports', 'Cooking', 'Movies'];
 
-  constructor() { }
+  interests = new FormControl();
+  interestList: string[] =["Java", "Python", "JavaScript", "Django","GoLang"]
+  fileToUpload: File = null;
+  url: string="../../../assets/images/print.svg";
+
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
   }
+  // geerates formGroup, assign to login form of type formGroup
+  buildForm(email?:string){
+    this.loginForm = this.formBuilder.group({
+      email:[email, Validators.required],
+      password:['', Validators.required]
+    })
+  }
+  submitForm(){
+    console.log(this.loginForm.value)
+  }
+ get email(){
+    return this.loginForm.get('email');
+ }
+ get password() {
+  return this.loginForm.get('password');
+ }
 
+ editForm() {
+    this.buildForm('email')
+ }
+
+ handleFileInput(file: FileList) {
+  this.fileToUpload = file.item(0);
+  // Show image preview
+  var reader = new FileReader();
+  reader.onload = (event:any) => {
+    this.url = event.target.result;
+  }
+  reader.readAsDataURL(this.fileToUpload);
+}
 }
